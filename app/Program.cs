7412f3a06606667b2f71a09348d06def542ef548
Александар Стефановић <stefanovicsalex@gmail.com>
2025-11-09 16:08:38 +0100

    Removed the expression tree traverser helper class and created the optimizer class with fully functional optimize method

--- app/Program.cs
+++ app/Program.cs
@@ -1,39 +1,63 @@
-﻿using ExpressionOptimizer;
-using Expressions;
+﻿using Expressions;
+static void PrintExpression(IExpression? expr, string indent = "")
+{
+    if (expr == null) return;
 
-// Create the variable 'x'
-IExpression variableX = new VariableExpression("x");
+    Console.WriteLine($"{indent}{expr} (Hash: {expr.GetHashCode()})");
 
-// 2 + x
-IExpression twoPlusX = new BinaryExpression(
+    if (expr is IBinaryExpression bin)
+    {
+        PrintExpression(bin.Left, indent + "  ");
+        PrintExpression(bin.Right, indent + "  ");
+    }
+    else if (expr is IFunction func)
+    {
+        PrintExpression(func.Argument, indent + "  ");
+    }
+}
+
+// (2 + x)
+IExpression twoPlusX1 = new BinaryExpression(
     new ConstantExpression(2),
-    variableX,
+    new VariableExpression("x"),
+    OperatorSign.Plus
+);
+
+IExpression twoPlusX2 = new BinaryExpression(
+    new ConstantExpression(2),
+    new VariableExpression("x"),
     OperatorSign.Plus
 );
 
 // 7 * (2 + x)
-IExpression sevenTimesTwoPlusX = new BinaryExpression(
+IExpression sevenTimesTwoPlusX1 = new BinaryExpression(
     new ConstantExpression(7),
-    twoPlusX,
+    twoPlusX1,
+    OperatorSign.Multiply
+);
+
+IExpression sevenTimesTwoPlusX2 = new BinaryExpression(
+    new ConstantExpression(7),
+    twoPlusX2,
     OperatorSign.Multiply
 );
 
 // sin(7 * (2 + x))
 IExpression sinExpression = new Function(
     FunctionKind.Sin,
-    sevenTimesTwoPlusX
+    sevenTimesTwoPlusX1
 );
 
 // cos(x)
 IExpression cosExpression = new Function(
     FunctionKind.Cos,
-    variableX
+    new VariableExpression("x")
 );
 
 // sin(7*(2+x)) - 7*(2+x)
 IExpression binaryExpression = new BinaryExpression(
     sinExpression,
-    sevenTimesTwoPlusX,
+    sevenTimesTwoPlusX2, // separate instance
     OperatorSign.Minus
 );
 
@@ -44,13 +68,11 @@ IExpression finalExpression = new BinaryExpression(
     OperatorSign.Plus
 );
 
-//Console.WriteLine(finalExpression);
-// Output: ((sin(7 * (2 + x)) - 7 * (2 + x)) + cos(x))
+Console.WriteLine("Original expression:");
+PrintExpression(finalExpression);
 
-ExpressionTreeTraverser traverser = new ExpressionTreeTraverser(finalExpression);
+// Optimize
+IExpression? optimized = ExpressionOptimizer.Optimize(finalExpression);
 
-IExpression? expr;
-while ((expr = traverser.next()) != null)
-{
-    Console.WriteLine(expr);
-}
+Console.WriteLine("\nOptimized expression:");
+PrintExpression(optimized);
--- lib/Expressions.cs
+++ lib/Expressions.cs
@@ -1,6 +1,6 @@
 namespace Expressions
 {
-    interface IExpression { IExpression? LeftExpression { get; } IExpression? RightExpression { get; } };
+    interface IExpression;
     interface IConstantExpression : IExpression { int Value { get; } }
     interface IVariableExpression : IExpression { string Name { get; } }
     interface IBinaryExpression : IExpression { IExpression Left { get; } IExpression Right { get; } OperatorSign Sign { get; } }
@@ -11,7 +11,7 @@ namespace Expressions
 
     class ConstantExpression : IConstantExpression
     {
-        public int Value { get; }        
+        public int Value { get; }
 
         public ConstantExpression(int value = 0)
         {
@@ -19,10 +19,6 @@ namespace Expressions
         }
 
         public override string ToString() => Value.ToString();
-
-        public IExpression? LeftExpression => null;
-
-        public IExpression? RightExpression => null;
     }
 
     class VariableExpression : IVariableExpression
@@ -35,10 +31,6 @@ namespace Expressions
         }
 
         public override string ToString() => Name;
-
-        public IExpression? LeftExpression => null;
-
-        public IExpression? RightExpression => null;
     }
 
     class BinaryExpression : IBinaryExpression
@@ -56,7 +48,7 @@ namespace Expressions
 
         public override string ToString() => $"({Left} {ToString(Sign)} {Right})";
 
-        private string ToString(OperatorSign sign)
+        protected string ToString(OperatorSign sign)
         {
             switch(sign)
             {
@@ -68,13 +60,9 @@ namespace Expressions
 
                 case OperatorSign.Divide: return "/";
 
-                default: throw new Exception();
+                default: throw new Exception("Invalid operator sign");
             }
         }
-
-        public IExpression? LeftExpression => Left;
-
-        public IExpression? RightExpression => Right;
     }
 
     class Function : IFunction
@@ -91,22 +79,43 @@ namespace Expressions
 
         public override string ToString() => $"{ToString(Kind)}({Argument})";
 
-        private string ToString(FunctionKind kind)
+        protected string ToString(FunctionKind kind) => kind.ToString().ToLower();
+    }
+
+    class ExpressionOptimizer
+    {
+        public static IExpression? Optimize(IExpression? expression)
         {
-            switch (kind)
-            {
-                case FunctionKind.Sin: return "sin";
+            if (expression == null) return null;
 
-                case FunctionKind.Cos: return "cos";
-
-                case FunctionKind.Max: return "max";
-
-                default: throw new Exception();
-            }
+            Dictionary<string, IExpression> cache = new Dictionary<string, IExpression>();
+            IExpression optimizedExpression = RemoveDuplicates(expression, cache);
+            return optimizedExpression;
         }
 
-        public IExpression? LeftExpression => Argument;
+        protected static IExpression RemoveDuplicates(IExpression expression, Dictionary<string, IExpression> cache)
+        {
+            string key = expression.ToString()!;
+            if (cache.TryGetValue(key, out IExpression? existingExpression)) return existingExpression;
 
-        public IExpression? RightExpression => null;
+            IExpression newExpression = CloneExpression(expression, cache);
+
+            cache[key] = newExpression;
+            return newExpression;
+        }
+
+        protected static IExpression CloneExpression(IExpression expression, Dictionary<string, IExpression> cache)
+        {
+            IExpression clonedExpression = expression switch
+            {
+                ConstantExpression c => new ConstantExpression(c.Value),
+                VariableExpression v => new VariableExpression(v.Name),
+                BinaryExpression b => new BinaryExpression(RemoveDuplicates(b.Left, cache), RemoveDuplicates(b.Right, cache), b.Sign),
+                Function f => new Function(f.Kind, RemoveDuplicates(f.Argument, cache)),
+                _ => throw new Exception("Invalid expression type")
+            };
+
+            return clonedExpression;
+        }
     }
 }
